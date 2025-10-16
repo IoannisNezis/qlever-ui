42f2c4d1a4a59df2544ce71a8ac8b38ce2b0e6c1
Ioannis Nezis <ioannis@nezis.de>
2025-10-16 19:08:07 +0200

    setup new frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+
+import tailwindcss from '@tailwindcss/vite';
+import wasm from 'vite-plugin-wasm';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+    outDir: './backend/static/editor/',
+    assetsInlineLimit: 0,
+  },
+  optimizeDeps: {
+    include: [
+      'vscode-textmate',
+      'vscode-oniguruma'
+    ]
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  plugins: [tailwindcss()],
+  assetsInclude: ['**/*yaml'],
+  worker: {
+    format: 'es',
+    plugins: () => [wasm()],
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  base: "/static/editor/",
   build: {
     rollupOptions: {
       input: 'qlever_ui/main.ts',
-      output: {
-        entryFileNames: 'editor.js',
-      },
+      // /index.htmlutput: {
+      //   entryFileNames: 'editor.js',
+      // },
     },
     outDir: './backend/static/editor/',
     assetsInlineLimit: 0,
@@ -20,7 +19,6 @@ export default defineConfig({
     plugins: () => [
       wasm(),
     ],
-
   }
 })
 
